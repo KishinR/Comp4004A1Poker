a3912ca6810bcf99152e781833f246b1dd55c773
Roman Kishinevsky <roman.kishinevsky@gmail.com>
2018-10-09 20:05:26 -0400

    method20
    functionality for royale flush

--- src/test/java/test/java/Hand.java
+++ src/test/java/test/java/Hand.java
@@ -368,7 +368,42 @@ public class Hand {
 		
 	}
 
-
+	public boolean royalFlush(String[] hand) {
+		LinkedList<String> suit = new LinkedList<String>();
+		LinkedList<String> rank = new LinkedList<String>();
+		for (int i = 0; i < hand.length; i++) {
+			suit.add(hand[i].substring(0, 1));
+			rank.add(hand[i].substring(1));
+	
+		}
+		
+		for (int i = 0; i < suit.size(); i++) {
+			for (int j = 0; j < suit.size(); j++) {
+				if (!suit.get(i).equals(suit.get(j))) 
+					return false;
+			}
+		}
+		
+		LinkedList<Integer> intType = new LinkedList<Integer>();
+		for (int i = 0; i < rank.size(); i++) {
+			if (rank.get(i).equals("J")) intType.add(11);
+			else if (rank.get(i).equals("Q")) intType.add(12);
+			else if (rank.get(i).equals("K")) intType.add(13);
+			else if (rank.get(i).equals("A")) intType.add(14);
+			else intType.add(Integer.parseInt(rank.get(i))); 
+		}
+		
+		Collections.sort(intType);
+		for (int i = 0; i < intType.size() - 1; i++) {
+			if (intType.get(i + 1) - intType.get(i) != 1)
+				return false;
+		}
+		
+		if (intType.get(0) != 10 && intType.get(4) != 14)
+			return false;
+		
+		return true;
+	}
 
 
 
